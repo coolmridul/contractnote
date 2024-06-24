8fdd25161403e0060018aa38a95ed2e7d07d7f9d
coolmridul <mridulagarwal8@gmail.com>
2024-06-24 19:20:34 +0530

    0.0.3

---
+++ commodities.py
@@ -0,0 +1,135 @@
+import re
+import pdfplumber
+import pandas as pd
+import numpy as np
+from decimal import Decimal, ROUND_HALF_UP, ROUND_FLOOR, ROUND_CEILING
+
+
+collection_of_items = {
+    'SILVERM':5,
+    'ALUMINI': 1000
+}
+
+def float_to_decimal(value):
+    return Decimal(value).quantize(Decimal('0.1'), rounding=ROUND_CEILING)
+
+def pdf_to_excel_commodities(abc):
+    with pdfplumber.open(abc) as pdf:
+        df1 = pd.DataFrame()
+        #len(pdf.pages)
+        for j in range(len(pdf.pages)):
+            page = pdf.pages[j]
+            text = page.extract_text()
+            # print(text)
+            pay_in = 0
+            pay_net = 0
+            gst = 0
+            sebi = 0
+            stamp = 0
+            transaction = 0
+            ctt = 0
+            date_bill = "NA"
+            line_items = []
+            df = pd.DataFrame()
+            # inv_line_re = re.compile(r'(\d{15})\s(\d{2}:\d{2}:\d{2})\s(\d{9})\s(\d{2}:\d{2}:\d{2})\s([A-Z])\s([A-Z]+)\s(\d{2}\/\d{2}\/\d{2})\s(Buy|Sell)\s(\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(-?\d+\.\d+)')
+            inv_line_re = re.compile(r'(\d{15})\s(\d{2}:\d{2}:\d{2})\s(\d{9})\s(\d{2}:\d{2}:\d{2})\s([A-Z])\s([A-Z]+)\s(\d{2}\/\d{2}\/\d{2})\s(Buy|Sell)\s(-?\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(\d+\.\d+)\s(-?\d+\.\d+)')
+
+            for line in text.split('\n'):
+                if "INTEGRATED GST @ 18%" in line:
+                    gst = float(line.split('INTEGRATED GST @ 18% ',1)[1])
+                if "SEBI FEES" in line:
+                    sebi = float(line.split('SEBI FEES ',1)[1])
+                if "STAMP DUTY" in line:
+                    stamp = float(line.split('STAMP DUTY ',1)[1])
+                if "TRANSACTIO CHARGES" in line:
+                    transaction = float(line.split('TRANSACTIO CHARGES ',1)[1])
+                if "CTT CHARGES" in line:
+                    ctt = float(line.split('CTT CHARGES ',1)[1])
+                if "PayIn/Payout Obligation" in line:
+                    pay_in = float(line.split('PayIn/Payout Obligation ',1)[1])
+                if "Net amount Receivable(-) | Payable(+) By Client" in line:
+                    pay_net = float(line.split('Net amount Receivable(-) | Payable(+) By Client ',1)[1])
+                if "Bill Date" in line:
+                    date_bill = line.split(" ")[3]
+                
+                line2 = inv_line_re.search(line)
+                if line2:
+                    name = line2.group(6)
+                    expiry = line2.group(7)
+                    buy_sell = line2.group(8)
+                    quantity = line2.group(9)
+                    price = line2.group(10)
+                    brokerage = line2.group(11)
+                    stock_original_name = line2.group(6) + " " + line2.group(7) + " " + date_bill + " " + line2.group(8)
+                    price_with_brokerage = line2.group(12)
+                    line_items.append({"stock_name":name,"stock_original_name":stock_original_name,"expiry":expiry,"buy_sell":buy_sell,
+                                    "quantity":int(quantity),"price":float(price),
+                                    "brokerage":float(brokerage),
+                                    "fprice":float(price_with_brokerage)})
+            
+            if len(line_items):
+                df = pd.DataFrame(line_items)
+            else:
+                df['stock_name'] = "NA"
+                df['stock_original_name'] = "NA"
+                df['buy_sell'] = "NA"
+                df['quantity'] = "NA"
+                df['price'] = "NA"
+                df['brokerage'] = "NA"
+                df['fprice'] = "NA"
+            
+            df['date_of_day'] = date_bill
+            df['page_number'] = j
+            df['charges'] = round(pay_net - pay_in,2)
+            df['g_gst'] = gst
+            df['g_sebi'] = sebi
+            df['g_stamp'] = stamp
+            df['g_trans'] = transaction
+            df['g_stt'] = ctt
+
+        
+            df1 = pd.concat([df1,df],axis=0)
+    
+    df1 = df1.reset_index(drop=True)
+    df1['lotsize'] = df1["stock_name"].apply(lambda x: collection_of_items.get(x))
+    df1['c_sebi'] = df1['price']*df1['quantity'].abs()*df1['lotsize']/1000000
+    df1['c_stamp'] = np.where(df1['buy_sell'] == 'Buy',df1['fprice']*df1['quantity'].abs()*df1['lotsize']*0.00002,0)
+    df1['c_stt'] = np.where(df1['buy_sell'] == 'Sell',df1['price']*df1['quantity'].abs()*df1['lotsize']*0.0001,0)
+    df1['c_trans'] = df1['price']*df1['quantity'].abs()*df1['lotsize']*0.000026
+    df1['c_gst'] = (df1['c_trans']+df1['c_sebi']+(df1['brokerage']*df1['lotsize']*df1['quantity'].abs()))*0.18
+    df1['c_charges'] = df1['c_sebi'] + df1['c_stamp'] + df1['c_stt'] + df1['c_trans'] + df1['c_gst']
+    
+    df1['c_stamp'] = df1['c_stamp'].apply(float_to_decimal)
+    df1['c_stamp'] = df1['c_stamp'].map(lambda x: x.quantize(Decimal('0.1'), ROUND_CEILING))
+
+    cols_to_round = ['c_sebi','c_stt','c_trans','c_gst','c_charges']
+    df1[cols_to_round] = df1[cols_to_round].astype(float)
+    df1['c_stamp'] = df1['c_stamp'].astype(float)
+    df1[cols_to_round] = df1[cols_to_round].round(4)
+    df1['g_charges'] = df1['g_sebi'] + df1['g_stamp'] + df1['g_stt'] + df1['g_trans'] + df1['g_gst']
+
+    df3 = df1.copy()
+    df3['quantity'] = df3['quantity'].abs()
+    df3['try'] = df3['quantity'] * df3['fprice']
+    df4 = df3.groupby(['stock_original_name'],sort=False).agg({'try': 'sum',
+         'quantity': 'sum',
+         'c_charges': 'sum',
+         'g_charges': 'mean'
+         }).reset_index()
+    
+    df4['final_price'] = df4['try']/df4['quantity']
+    df4 = df4.drop(['try'],axis=1)
+    df4[['Stock', 'Expiry', 'Date', 'Buy_Sell']] = df4['stock_original_name'].str.split(' ', expand=True)
+
+    df4 = df4[['Buy_Sell','Date','Stock','Expiry','quantity','final_price','g_charges','c_charges']]
+
+    # writer = pd.ExcelWriter('roshan.xlsx', engine='xlsxwriter')
+    # df1.to_excel(writer,sheet_name='Complete Data',index=False)
+    # df4.to_excel(writer,sheet_name='Short',index=False)
+    # writer.save()
+
+    return df4
+
+
+
+# pdf_to_excel_commodities()
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,38 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from stocks import *
-
+from commodities import *
 
 @st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode("utf-8")
 
-uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=False)
-if uploaded_files is not None:
-    df_new = pdf_to_excel_equity(uploaded_files)
-    df_new = df_new.fillna("NA")
-    st.dataframe(df_new)
-    csv_data = convert_df(df_new)
-    btn = st.download_button(
-    label="Download",
-    data=csv_data,
-    file_name="Stock Summary.csv",
-    mime="text/csv",)
+option = st.selectbox(
+   "Select",
+   ("Equity", "Commodities")
+)
+if option == 'Equity':
+    uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=False)
+    if uploaded_files is not None:
+        df_new = pdf_to_excel_equity(uploaded_files)
+        df_new = df_new.fillna("")
+        st.dataframe(df_new)
+        csv_data = convert_df(df_new)
+        btn = st.download_button(
+        label="Download",
+        data=csv_data,
+        file_name="Stock Summary.csv",
+        mime="text/csv",)
+if option == 'Commodities':
+     uploaded_files1 = st.file_uploader("Choose a PDF file", accept_multiple_files=False)
+     if uploaded_files1 is not None:
+        df_new = pdf_to_excel_commodities(uploaded_files1)
+        df_new = df_new.fillna("")
+        st.dataframe(df_new)
+        csv_data = convert_df(df_new)
+        btn = st.download_button(
+        label="Download",
+        data=csv_data,
+        file_name="Commodity Summary.csv",
+        mime="text/csv",)
